d04b9234a480df93ba9f167d0a2dcc6f6a55d5be
= <=>
2012-03-25 19:17:03 +1100

    Testing eclipse git plugin.

--- src/com/alexanderbatten/bluetoothfriends/ViewAllDevices.java
+++ src/com/alexanderbatten/bluetoothfriends/ViewAllDevices.java
@@ -163,7 +163,7 @@ public class ViewAllDevices extends ListActivity {
 	        deviceName.setText(device.getCustomOrNameOrMac());
 	        
 	        deviceRelationshipStatus = (TextView) row.findViewById(R.id.devicelistrow_relationshipStatus);
-	        deviceRelationshipStatus.setText("lol");
+	        deviceRelationshipStatus.setText("change");
 	        
 	        deviceRelationshipProgress = (ProgressBar) row.findViewById(R.id.devicelistrow_relationshipProgress);
 	        deviceRelationshipProgress.setMax(100);
